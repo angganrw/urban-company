f0f0ee386ea6bea51040fed02bee242698a7d392
ANGGA NIRWANA <mas_anggaa@yahoo.com>
2024-10-13 01:08:02 +0700

    change padding

--- src/view/pages/services/index.tsx
+++ src/view/pages/services/index.tsx
@@ -55,7 +55,7 @@ export default async function ServiceView() {
               {services.fields.description}
             </p>
             <Link href="/our-teams">
-              <button className="relative -top-1 -left-1 bg-[#1a79af] py-2.5 px-5 font-medium uppercase text-white transition-all before:absolute before:top-1 before:left-1 before:-z-[1] before:h-full before:w-full before:border-2 before:border-gray-700 before:transition-all before:content-[''] hover:top-0 hover:left-0 before:hover:top-0 before:hover:left-0">
+              <button className="relative -top-1 -left-1 bg-[#1a79af] py-4 px-6 font-medium uppercase text-white transition-all before:absolute before:top-1 before:left-1 before:-z-[1] before:h-full before:w-full before:border-2 before:border-gray-700 before:transition-all before:content-[''] hover:top-0 hover:left-0 before:hover:top-0 before:hover:left-0">
                 Lets Talk
               </button>
             </Link>

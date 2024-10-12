873236625ad13e708b470be90911bbba05223ee8
ANGGA NIRWANA <mas_anggaa@yahoo.com>
2024-10-13 01:13:08 +0700

    padding 2

--- src/view/pages/services/index.tsx
+++ src/view/pages/services/index.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Link from "next/link";
 import { TiThMenu } from "react-icons/ti";
 import ContentfulClient from "@/Contentful/contentfulClient";
 import {
@@ -54,11 +53,9 @@ export default async function ServiceView() {
             <p className="text-gray-900 text-sm md:text-base mb-8 md:mb-10 text-center">
               {services.fields.description}
             </p>
-            <Link href="/our-teams">
-              <button className="relative -top-1 -left-1 bg-[#1a79af] py-4 px-6 font-medium uppercase text-white transition-all before:absolute before:top-1 before:left-1 before:-z-[1] before:h-full before:w-full before:border-2 before:border-gray-700 before:transition-all before:content-[''] hover:top-0 hover:left-0 before:hover:top-0 before:hover:left-0">
-                Lets Talk
-              </button>
-            </Link>
+            <button className="relative -top-1 -left-1 bg-[#1a79af] py-2.5 px-5 font-medium uppercase text-white transition-all before:absolute before:top-1 before:left-1 before:-z-[1] before:h-full before:w-full before:border-2 before:border-gray-700 before:transition-all before:content-[''] hover:top-0 hover:left-0 before:hover:top-0 before:hover:left-0">
+              Lets Talk
+            </button>
           </div>
         ))}
       </div>

bd418a77fd974ce6a6033bc223d39aca96d98c99
ANGGA NIRWANA <mas_anggaa@yahoo.com>
2024-10-14 23:03:34 +0700

    test seo jadi turun hbs update

--- src/view/pages/services/index.tsx
+++ src/view/pages/services/index.tsx
@@ -56,7 +56,7 @@ export default async function ServiceView() {
             </p>
             <Link href={`/services/${services.fields.slug}`}>
               <button className="relative -top-1 -left-1 bg-[#1a79af] py-2.5 px-5 font-medium uppercase text-white transition-all before:absolute before:top-1 before:left-1 before:-z-[1] before:h-full before:w-full before:border-2 before:border-gray-700 before:transition-all before:content-[''] hover:top-0 hover:left-0 before:hover:top-0 before:hover:left-0">
-                Learn More
+                Explore our {services.fields.title} Services
               </button>
             </Link>
           </div>
